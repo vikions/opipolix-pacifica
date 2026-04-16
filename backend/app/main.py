6b078b8071609491be8b73382cebf6083a4fece3
vikions <plambir1994@mail.ru>
2026-04-16 12:11:03 +0200

    fix: site_one1

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,7 +14,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://opipolix-pacifica.vercel.app/",
+        "https://opipolix-pacifica.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
